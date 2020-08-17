3f244431b166c771463c421068692b44adf5b929
Lee Dong Bok <ehdqhr3408@naver.com>
2020-08-17 18:35:06 +0900

    word chain part1

--- .idea/Word Chain.js
+++ .idea/Word Chain.js
@@ -1,4 +1,11 @@
-var word = '제로초'
+var 바디 = document.body;
+var 단어 = document.createElement('div');
+단어.textContent='제로초'
+document.body.append(단어);
+var 결과창 = document.createElement('div');
+document.body.append(결과창);
+
+/* var word = '제로초'
 while(true){
     var newword = prompt(word);
     if(word[word.length-1]===newword[0]){
@@ -8,4 +15,4 @@ while(true){
     else{
         alert("땡");
     }
-}
+} */
